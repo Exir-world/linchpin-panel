3263247639f7b41b970370d507a9df4f4e90ec81
informatiker99 <hosseincybercoin@gmail.com>
2025-04-05 15:44:49 +0330

    add: add filter in requests page and fix lang

--- src/app/[locale]/(app)/requests/page.tsx
+++ src/app/[locale]/(app)/requests/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+"use client";
+import CustomDropdown from "@/components/dropdown/dropdown";
+import { useTranslations } from "next-intl";
+import React from "react";
 
 const Requests = () => {
-  return (
-    <div>Requests</div>
-  )
-}
+  const t = useTranslations();
+  const requestTypes = [
+    {
+      label: t("global.requests.filter.confirmed"),
+      key: t("global.requests.filter.confirmed"),
+    },
+    {
+      label: t("global.requests.filter.rejected"),
+      key: t("global.requests.filter.rejected"),
+    },
+    {
+      label: t("global.requests.filter.not-registered"),
+      key: t("global.requests.filter.not-registered"),
+    },
+  ];
 
-export default Requests
+  const handleChange = (val: string) => {
+    console.log(val);
+  };
+
+  return (
+    <div>
+      <div className="flex full p-2 ">
+        <div className="flex items-center p-2 gap-3">
+          <span>{t("global.requests.status")}</span>
+          <CustomDropdown
+            dropdownItems={requestTypes}
+            onChange={(val) => handleChange(val)}
+          ></CustomDropdown>
+        </div>
+      </div>
+      Requests
+    </div>
+  );
+};
+
+export default Requests;
