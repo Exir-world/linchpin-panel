28c8c25890742eb6cba05b471daf120f8848c2eb
informatiker99 <hosseincybercoin@gmail.com>
2025-05-10 15:15:29 +0330

    fix: modify middleware file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,23 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
 
-export default createMiddleware(routing);
+// export default createMiddleware(routing);
 
+const intlMiddleware = createMiddleware(routing);
+
+export const middleware = (req: NextRequest) => {
+  const intlResponse = intlMiddleware(req);
+  if (intlResponse) {
+    return intlResponse;
+  }
+
+  const cookies = req.cookies.get("linchpin-admin");
+  if (!cookies?.value) {
+    const locale = req.nextUrl.locale || "en";
+    return NextResponse.redirect(new URL(`/${locale}/login`, req.url));
+  }
+};
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
